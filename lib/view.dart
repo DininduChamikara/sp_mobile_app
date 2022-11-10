504d4ccad0f47c7bc4ffdf623e39e5df6742204c
Sithum Basnayaka <sithum.dashantha@gmail.com>
2022-11-10 22:12:27 +0530

    made login screen

---
+++ lib/view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ViewPage extends StatelessWidget {
+  const ViewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: Column(
+        children: [
+          Row(
+            children: const [
+              Text("Add Data"),
+              Icon(Icons.close),
+            ],
+          ),
+          FundContent()
+        ],
+      ),
+    );
+  }
+}
+
+class FundContent extends StatelessWidget {
+  const FundContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Text("Amount"),
+        Text("Rs.100"),
+        Text("Rs.100"),
+      ],
+    );
+  }
+}
