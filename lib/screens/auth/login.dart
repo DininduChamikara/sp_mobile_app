30e0861a33188037c90f46cbae8db3774182fd5b
dinindu99 <peiristd-im18057@stu.kln.ac.lk>
2022-11-12 20:32:11 +0530

    upto username field done

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -8,8 +8,54 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.brown,
-      child: const Text("Hello World"),
-    );
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: (Text(
+                "Let's sign you in.",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              )),
+            ),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: (Text(
+                "Welcome back.",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.red,
+                ),
+              )),
+            ),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: (Text(
+                "You've been missed!",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.red,
+                ),
+              )),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 59, 81, 118),
+                  borderRadius: BorderRadius.circular(10)),
+              height: 60,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: const TextField(
+                decoration: InputDecoration(
+                    hintText: "Phone, email or username",
+                    border: InputBorder.none),
+              ),
+            )
+          ],
+        ));
   }
 }
