090e7a5f026d8905dc9f9f16cb24a1cbec719e07
dinindu99 <peiristd-im18057@stu.kln.ac.lk>
2022-11-18 09:06:38 +0530

    floating action btn tested in bills

--- lib/screens/main/bills.dart
+++ lib/screens/main/bills.dart
@@ -5,6 +5,11 @@ class Bills extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
