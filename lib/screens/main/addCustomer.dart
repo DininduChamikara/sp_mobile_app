7ab0c4621c87250c51989913845e8834977c88b4
dinindu99 <peiristd-im18057@stu.kln.ac.lk>
2022-11-25 23:55:16 +0530

    Add cusmoner page cancel button fixed

--- lib/screens/main/addCustomer.dart
+++ lib/screens/main/addCustomer.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unnecessary_const
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:sp_mobile_app/components/title.dart';
 
 class AddCustomer extends StatelessWidget {
@@ -10,15 +11,15 @@ class AddCustomer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(elevation: 0, title: const Text("Add Customer")),
-      body: Padding(
+      body: Container(
         padding: const EdgeInsets.all(24),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            MainTitle(title: "Customers"),
-            Align(
+          children: [
+            const MainTitle(title: "Add Customer"),
+            const Align(
               child: Padding(
-                padding: const EdgeInsets.all(1),
+                padding: EdgeInsets.all(1),
               ),
             ),
             const Spacer(),
@@ -63,6 +64,37 @@ class AddCustomer extends StatelessWidget {
                 decoration: InputDecoration(hintText: "Credit"),
               ),
             ),
+            const Spacer(),
+            Row(
+              children: [
+                Expanded(
+                    child: Container(
+                  padding: const EdgeInsets.only(right: 4.0),
+                  child: ElevatedButton(
+                    child: const Text("Cancel"),
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      elevation: 0,
+                    ),
+                    onPressed: () {
+                      return context.go('/');
+                    },
+                  ),
+                )),
+                Expanded(
+                    child: Container(
+                  padding: const EdgeInsets.only(left: 4.0),
+                  child: ElevatedButton(
+                    child: const Text("Send"),
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      elevation: 0,
+                    ),
+                    onPressed: () {},
+                  ),
+                ))
+              ],
+            )
           ],
         ),
       ),
